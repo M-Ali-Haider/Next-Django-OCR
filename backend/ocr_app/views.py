4cc8adc431db51f9c5bfd6265708ca27e50a96fc
M-Ali-Haider <mah01134212100@gmail.com>
2024-12-18 20:11:04 +0500

    added requirements.txt

--- backend/ocr_app/views.py
+++ backend/ocr_app/views.py
@@ -122,7 +122,8 @@ def process_image(request):
             # text = SimpleOCR.image_to_string(image)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-            gray = cv2.medianBlur(gray, 3)            
+            gray = cv2.medianBlur(gray, 3)      
+                  
             text = pytesseract.image_to_string(gray)
             return JsonResponse({
                 'success': True,
