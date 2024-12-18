354f266c46a0ade8c13e47c663a34272991f1d82
MHamzaFarooq <hamzafarooq49ml@gmail.com>
2024-12-18 20:35:57 +0500

    Button Updated

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -2,7 +2,7 @@ const Button = ({ Icon, label, className, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`${className} bg-white border border-[#546fff] rounded-[4px] text-[#546fff] uppercase text-sm font-medium px-8 py-3 active:scale-95 duration-150 transition-all ease-linear`}
+      className={`${className} bg-white border border-[#546fff] rounded-[8px] text-[#546fff] uppercase text-sm font-medium px-8 py-3 active:scale-95 duration-150 transition-all ease-linear`}
     >
       {label}
     </button>
